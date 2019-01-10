455779f095743e3d0b1a5858d3a41811516cc5e1
ShellyHsueh <hsueh821203@gmail.com>
2019-01-10 12:11:31 +0800

    Checklist popover & Sortable items

--- web/html_dependencies.php
+++ web/html_dependencies.php
@@ -13,12 +13,24 @@ function echoBootstrapAndJqueryDependencies() {
           integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- The default jquery slim version for bootstrap not including $.ajax/$.post/$.get, so use the full version -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    
+    <!-- Jquery sortable -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha384-PtTRqvDhycIBU6x1wwIqnbDo8adeWIWP3AHmnrvccafo35E7oIvW7HPXn2YimvWu" crossorigin="anonymous"></script>
+  ');
+}
 
-    <!-- jquery slim version is for bootstrap, but no $.ajax/$.post/$.get in slim version -->
-    <script src="libraries/jquery-3.3.1.min.js"></script>
+
+// Echo jquery slim in the bottom of html (to avoid conflict with other jquery versions)
+function echoJquerySlimForBootstrap() {
+  echo ('
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   ');
 }
 
--- web/index.php
+++ web/index.php
@@ -6,9 +6,78 @@
 <head>
   <?php echoBootstrapAndJqueryDependencies(); ?>
   <link rel="stylesheet" href="assets/styles/styles.css">
+
+  <script src="assets/js/checklist.js"></script>
 </head>
 
 <body>
+
+<div class="container-fluid pt-3">
+  <div class="row">
+    <div class="col-9 col-md-5">
+      <div class="checklist-header">
+        <div class="row pl-2">
+          <span class="icon-span col-1 align-middle text-center">
+            <i class="far fa-check-square"></i>
+          </span>
+          <h6 class="col-8 checklist-title">Checklist</h6>
+        </div>
+      </div>
+      <hr class="checklist-division">
+
+      <div id="item-forms-area">
+        <!-- <li class="sortable-form"> -->
+          <form class="item-form">
+            <div class="form-check mt-2 row">
+              <input type="checkbox" class="shadow-sm mr-2">
+              <input type="text" class="content-input" placeholder="Add an item...">
+            </div>
+          </form>
+        <!-- </li> -->
+        
+        <!-- <li class="sortable-form"> -->
+          <form class="item-form">
+            <div class="form-check mt-2 row">
+              <input type="checkbox" class="shadow-sm mr-2">
+              <input type="text" class="content-input" placeholder="Add an item...">
+            </div>
+          </form>
+        <!-- </li> -->
+
+        <!-- <li class="sortable-form"> -->
+          <form class="item-form">
+            <div class="form-check mt-2 row">
+              <input type="checkbox" class="shadow-sm mr-2">
+              <input type="text" class="content-input" placeholder="Add an item...">
+            </div>
+          </form>
+        <!-- </li> -->
+
+        <!-- <li class="sortable-form"> -->
+          <form class="item-form">
+            <div class="form-check mt-2 row">
+              <input type="checkbox" class="shadow-sm mr-2">
+              <input type="text" class="content-input" placeholder="Add an item...">
+            </div>
+          </form>
+        <!-- </li> -->
+
+      </div>
+
+    </div>
+
+    <div class="col-3 col-md-3 mt-2">
+      <button class="btn btn-light btn-sm shadow-sm checklist-popover-btn" data-toggle="popover" data-placement="bottom">
+        Checklist
+      </button>
+    <div>
+
+    
+  </div>
+  
+</div>
+
+
 </body>
 
 </html>
